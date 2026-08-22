1ced5c652912b792210a9fcfab5c6c36bc67a0ec
Aman Mahish <amanmahish73@gmail.com>
2026-08-22 15:27:50 +0530

    creating logout route fixed token store httponly cookies

--- backend/src/middleware/verification-middleware.ts
+++ backend/src/middleware/verification-middleware.ts
@@ -1,19 +1,24 @@
-import type { Request, Response, NextFunction } from "express";
+import type {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
 import jwt from "jsonwebtoken";
 export function authenticateJWT(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  const token = req.header("Authorization")?.split(" ")[1];
+  const token = req.cookies.accessToken;
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: "Token verification failed" });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
     req.user = decoded!;
     next();
-  });
+  } catch (error) {
+    return res.status(403).json({ message: "Token verification failed" });
+  }
 }
+
--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userController from "../controllers/auth-controller.js";
+import { authenticateJWT } from "../middleware/verification-middleware.js";
 const authRouter = Router();
 
 authRouter.post("/register", userController.register);
 authRouter.post("/login", userController.login);
+authRouter.post("/logout", authenticateJWT, userController.logout);
 
 export default authRouter;
