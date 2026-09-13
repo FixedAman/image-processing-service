e0b5a220d3444040ffa1922715a9998ff5365620
Aman Mahish <amanmahish73@gmail.com>
2026-09-13 17:48:43 +0530

    authetication part complete

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import { dbConnection } from "./db/database-connection.js";
-dotenv.config();
+dotenv.config({ debug: true });
 const PORT = process.env.PORT;
 
 function serverOn() {
